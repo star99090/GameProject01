fb7ad9c9b0d3a4e71cda128014fc14c5105026ef
star99090 <star99090@naver.com>
2020-05-11 00:05:12 +0900

    카메라 수정 필요, 이동 및 점프 애니메이션 추가

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Animator anim;
+    Rigidbody rigid;
+    public float charSpeed;
+    bool isJump;
+
+    void Awake()
+    {
+        isJump = false;
+        anim = GetComponent<Animator>();
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump") && !isJump){
+            anim.SetTrigger("jumpTrigger");
+            isJump = true;
+            rigid.AddForce(Vector3.up * 5, ForceMode.Impulse);
+        }
+    }
+
+    void OnCollisionEnter(Collision collision) // 충돌 시 생기는 이벤트
+    {
+        if (collision.gameObject.name == "Terrain") // 만약 이 스크립트가 적용된 오브젝트가 Terrain라는 오브젝트를 만나면 진행
+            isJump = false;
+    }
+
+    void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        anim.SetFloat("h", h);
+        anim.SetFloat("v", v);
+
+        if (h == 0.0f && v == 0.0f)
+            anim.SetBool("isRunning", false);
+        else
+        {
+            rigid.AddForce(new Vector3(h * charSpeed, 0, v * charSpeed), ForceMode.Impulse);
+            anim.SetBool("isRunning", true);
+        }
+
+    }
+}
---
+++ Assets/Scripts/cameraMove.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraMove : MonoBehaviour
+{
+    GameObject target;
+    float moveSpeed;
+    Vector3 targetPosition;
+
+    void Awake()
+    {
+        target = GetComponent<GameObject>();
+    }
+    void Update()
+    {
+        targetPosition.Set(target.transform.position.x, target.transform.position.y, this.transform.position.z);
+        this.transform.position = Vector3.Lerp(this.transform.position, targetPosition, moveSpeed * Time.deltaTime);
+    }
+}
